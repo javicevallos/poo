e040913fe3cc03462a9a84b5395bb38863e49931
“javiercevallos@hotmail.com” <javicevallos@hotmail.com>
2023-05-07 18:33:00 -0500

    Primera carga de las practicas JAVA

---
+++ 1/POO_Variables/src/main/java/com/mycompany/poo_variables/POO_Variables.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poo_variables;
+
+/**
+ *
+ * @author WILMER
+ */
+public class POO_Variables {
+
+    /*
+        Crea una variable de cada tipo:
+            - byte
+            - short
+            - int
+            - long
+            - char
+            - double
+            - float
+            - String
+            - boolean
+    
+    */
+    public static void main(String[] args) {
+       
+        //numeros enteros
+        byte a=5;
+        short b = 200;
+        int c = 1000000;
+        long d = 10000000000L;
+        
+        //caracter
+        char e = 'a';
+        
+        double f = 4.5;
+        float g = 4.5f;
+        
+        String h = "Hola mundo";
+        
+        boolean i = 5 > 10;
+        
+        System.out.println(a + "--" + b);
+    }
+    
+
+}
